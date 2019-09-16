fe212bf1762a0eecc98ce43eb98b1ec78c6126a0
1772012 <it1772012@student.it.maranatha.edu>
2019-09-16 20:41:22 +0700

    first commit

---
+++ src/com/cbtuser/controller/LoginViewController.java
@@ -0,0 +1,22 @@
+package com.cbtuser.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kafka Febianto Agiharta - 1772012
+ */
+public class LoginViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
