77c45c5599dc551f92998c3ab1284b08c877e4d4
1772012 <it1772012@student.it.maranatha.edu>
2019-09-21 00:20:20 +0700

    revisi ketiga

--- src/com/cbtuser/controller/LoginViewController.java
+++ src/com/cbtuser/controller/LoginViewController.java
@@ -2,7 +2,17 @@ package com.cbtuser.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -11,12 +21,42 @@ import javafx.fxml.Initializable;
  */
 public class LoginViewController implements Initializable {
 
+    @FXML
+    private GridPane root;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Pane pane;
+    @FXML
+    private ImageView imgCbt;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
+        //  Lock the imgCbt size to parent container
+        imgCbt.fitWidthProperty().bind(pane.widthProperty());
+        imgCbt.fitHeightProperty().bind(pane.heightProperty());
+
+        //  Disable the TextField focus
+        final BooleanProperty firstTime = new SimpleBooleanProperty(true);
+        txtUsername.focusedProperty().addListener((observable, oldValue,
+                newValue) -> {
+            if (newValue && firstTime.get()) {
+                root.requestFocus();
+                firstTime.setValue(false);
+            }
+        });
+    }
+
+    @FXML
+    private void btnLoginClick(ActionEvent event) {
     }
 
 }
